a3641766324257d2187c1c9046a2f1158eec615e
Mücahit Yıldız <mucahityyildiz@gmail.com>
2024-12-27 21:17:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+
+// React kütüphanesi oluşturma adımları
+/*
+- Terminali aç
+- Works klasörüne olduğuna emin ol
+- Eğer klasörü onun oluşturmasını istiyorsan ; 
+"npm create vite" yazmalısın
+- Eğer klasörü sen açacaksan ve içine oluşturmak istiyorsan;
+"npm create vite ./" yazmalısın
+- proje / klasör adını yazıp enter a bas
+- Framework seçiniz (React seçilecek)
+- Hangi dilde yazmak istiyorsan onu seç (JS seçilecek)
+- cd ile oluşturduğun klasörün içine gitmen gerekli
+"cd ornek-dosya-adi" + enter
+- içerisine vite yüklemek için "npm i ya da npm install" yazıp enter a bas
+- "npm run dev" ile localhost'unda çalıştırabilirsin
+*/
+
+/*
+npm crate vite
+klasör adı
+framework
+dil
+cd dosya-adi
+npm i
+npm run dev
+*/
